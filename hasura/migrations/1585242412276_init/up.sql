fef98ab7883e202d42947869e427bea9cd66e069
jsus <esseswann@gmail.com>
2020-04-05 20:58:47 +0300

    Backup migrations

--- hasura/migrations/1585242412276_init/up.sql
+++ hasura/migrations/1585242412276_init/up.sql
@@ -1,3 +1,14 @@
+CREATE FUNCTION public.set_current_timestamp_updated_at() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+DECLARE
+  _new record;
+BEGIN
+  _new := NEW;
+  _new."updated_at" = NOW();
+  RETURN _new;
+END;
+$$;
 CREATE TABLE public.vshift (
     date date NOT NULL,
     start time with time zone NOT NULL,
@@ -27,18 +38,63 @@ SELECT (days.date)::date AS date,
            FROM volunteer_shift
           GROUP BY volunteer_shift.date, volunteer_shift.start, volunteer_shift."end", volunteer_shift.hospital_id) vs_stat ON (((vs_stat.date = (days.date)::date) AND (vs_stat.start = period.start) AND (vs_stat."end" = period."end") AND vs_stat.hospital_id = period.hospital_id)))
   WHERE CASE WHEN hospital_ids IS NULL THEN TRUE ELSE period.hospital_id = ANY(hospital_ids) END 
-  GROUP BY ((days.date)::date), period.start, period."end";
+  GROUP BY ((days.date)::date), period.start, period."end"
+  ORDER BY date, period.start;
   $$;
+CREATE FUNCTION public.shift_selector2(hospital_ids text[] DEFAULT NULL::text[]) RETURNS SETOF public.vshift
+    LANGUAGE sql STABLE
+    AS $$ 
+SELECT (days.date)::date AS date,
+    period.start,
+    period."end",
+    count(DISTINCT period.hospital_id)::integer AS hospitalscount,
+    sum(period.demand)::integer AS demand,
+    (((sum(period.demand))::numeric - COALESCE(sum(vs_stat.subs), (0)::numeric)))::integer AS placesavailable,
+    gen_random_uuid() as uid
+   FROM ((period
+     LEFT JOIN generate_series((CURRENT_DATE)::timestamp without time zone, (CURRENT_DATE + '14 days'::interval), '1 day'::interval) days(date) ON (true))
+     LEFT JOIN ( SELECT volunteer_shift.date,
+            volunteer_shift.start,
+            volunteer_shift."end",
+            count(volunteer_shift.uid) AS subs,
+            hospital_id
+           FROM volunteer_shift
+          GROUP BY volunteer_shift.date, volunteer_shift.start, volunteer_shift."end", volunteer_shift.hospital_id) vs_stat ON (((vs_stat.date = (days.date)::date) AND (vs_stat.start = period.start) AND (vs_stat."end" = period."end") AND vs_stat.hospital_id = period.hospital_id)))
+  WHERE CASE WHEN hospital_ids IS NULL THEN TRUE ELSE period.hospital_id = ANY(hospital_ids::uuid[]) END 
+  GROUP BY ((days.date)::date), period.start, period."end"
+  ORDER BY date, period.start;
+  $$;
+CREATE TABLE public.hint (
+    uid uuid DEFAULT public.gen_random_uuid() NOT NULL,
+    text character varying NOT NULL,
+    name bpchar NOT NULL
+);
+CREATE FUNCTION public.unseen_hint(hasura_session json, name character) RETURNS SETOF public.hint
+    LANGUAGE sql STABLE
+    AS $$
+    SELECT hint.* FROM hint; --left outer join seen_hint ON seen_hint.hint_id = hint.uid where hint.name = name AND seen_hint.user_id = (hasura_session ->> 'x-hasura-user-id')::uuid
+$$;
+CREATE TABLE public.blacklist (
+    uid uuid DEFAULT public.gen_random_uuid() NOT NULL,
+    created_at timestamp with time zone DEFAULT now() NOT NULL,
+    updated_at timestamp with time zone DEFAULT now() NOT NULL,
+    volunteer_id uuid NOT NULL,
+    comment text NOT NULL
+);
 CREATE TABLE public.hospital (
     uid uuid DEFAULT public.gen_random_uuid() NOT NULL,
     name character varying NOT NULL,
     shortname character varying NOT NULL,
     phone character varying NOT NULL,
-    address character varying NOT NULL
+    address character varying NOT NULL,
+    created_at timestamp with time zone DEFAULT now() NOT NULL,
+    updated_at timestamp with time zone DEFAULT now() NOT NULL
 );
 CREATE TABLE public.hospital_coordinator (
     hospital_id uuid NOT NULL,
-    coophone text NOT NULL
+    coophone text NOT NULL,
+    created_at timestamp with time zone DEFAULT now() NOT NULL,
+    updated_at timestamp with time zone DEFAULT now() NOT NULL
 );
 CREATE VIEW public.coordinator_hospitals_view AS
  SELECT hospital_coordinator.coophone,
@@ -49,11 +105,6 @@ CREATE VIEW public.coordinator_hospitals_view AS
     hospital.address
    FROM (public.hospital_coordinator
      LEFT JOIN public.hospital ON ((hospital_coordinator.hospital_id = hospital.uid)));
-CREATE TABLE public.hint (
-    uid uuid DEFAULT public.gen_random_uuid() NOT NULL,
-    text character varying NOT NULL,
-    name bpchar NOT NULL
-);
 CREATE TABLE public.volunteer (
     uid uuid DEFAULT public.gen_random_uuid() NOT NULL,
     fname character varying NOT NULL,
@@ -61,26 +112,13 @@ CREATE TABLE public.volunteer (
     lname character varying NOT NULL,
     phone character varying NOT NULL,
     email character varying NOT NULL,
-    blacklisted boolean DEFAULT false NOT NULL,
-    blacklist_comment text NOT NULL,
     role character varying NOT NULL,
     password character varying DEFAULT ''::character varying NOT NULL,
-    hospital_id uuid
+    hospital_id uuid,
+    comment character varying,
+    created_at timestamp with time zone DEFAULT now() NOT NULL,
+    updated_at timestamp with time zone DEFAULT now() NOT NULL
 );
-CREATE VIEW public.hospital_coordinators_view AS
- SELECT hospital_coordinator.hospital_id,
-    volunteer.uid,
-    volunteer.fname,
-    volunteer.mname,
-    volunteer.lname,
-    volunteer.phone,
-    volunteer.email,
-    volunteer.blacklisted,
-    volunteer.blacklist_comment,
-    volunteer.role,
-    volunteer.password
-   FROM (public.hospital_coordinator
-     LEFT JOIN public.volunteer ON ((hospital_coordinator.coophone = (volunteer.phone)::text)));
 CREATE VIEW public.me AS
  SELECT volunteer.uid,
     volunteer.fname,
@@ -88,18 +126,21 @@ CREATE VIEW public.me AS
     volunteer.lname,
     volunteer.phone,
     volunteer.email,
-    volunteer.blacklisted,
-    volunteer.blacklist_comment,
     volunteer.role,
     volunteer.password,
-    volunteer.hospital_id
+    volunteer.hospital_id,
+    volunteer.comment,
+    volunteer.created_at,
+    volunteer.updated_at
    FROM public.volunteer;
 CREATE TABLE public.period (
     start time with time zone NOT NULL,
     "end" time with time zone NOT NULL,
     hospital_id uuid NOT NULL,
     demand integer NOT NULL,
-    uid uuid DEFAULT public.gen_random_uuid() NOT NULL
+    uid uuid DEFAULT public.gen_random_uuid() NOT NULL,
+    created_at timestamp with time zone DEFAULT now() NOT NULL,
+    updated_at timestamp with time zone DEFAULT now() NOT NULL
 );
 CREATE TABLE public.profession (
     name text NOT NULL
@@ -119,7 +160,9 @@ CREATE TABLE public.volunteer_shift (
     volunteer_id uuid NOT NULL,
     confirmed boolean DEFAULT false NOT NULL,
     uid uuid DEFAULT public.gen_random_uuid() NOT NULL,
-    hospital_id uuid NOT NULL
+    hospital_id uuid NOT NULL,
+    created_at timestamp with time zone DEFAULT now() NOT NULL,
+    updated_at timestamp with time zone DEFAULT now() NOT NULL
 );
 CREATE VIEW public.shifts AS
  SELECT (days.date)::date AS date,
@@ -144,8 +187,12 @@ CREATE TABLE public.special_shift (
     date date NOT NULL,
     hospital_id uuid NOT NULL,
     demand integer NOT NULL,
-    uid uuid DEFAULT public.gen_random_uuid() NOT NULL
+    uid uuid DEFAULT public.gen_random_uuid() NOT NULL,
+    created_at timestamp with time zone DEFAULT now() NOT NULL,
+    updated_at timestamp with time zone DEFAULT now() NOT NULL
 );
+ALTER TABLE ONLY public.blacklist
+    ADD CONSTRAINT blacklist_pkey PRIMARY KEY (uid);
 ALTER TABLE ONLY public.hint
     ADD CONSTRAINT hint_pkey PRIMARY KEY (name);
 ALTER TABLE ONLY public.hint
@@ -192,6 +239,22 @@ ALTER TABLE ONLY public.volunteer_shift
     ADD CONSTRAINT volunteer_shift_uid_key UNIQUE (uid);
 ALTER TABLE ONLY public.vshift
     ADD CONSTRAINT vshift_pkey PRIMARY KEY (uid);
+CREATE TRIGGER set_public_blacklist_updated_at BEFORE UPDATE ON public.blacklist FOR EACH ROW EXECUTE FUNCTION public.set_current_timestamp_updated_at();
+COMMENT ON TRIGGER set_public_blacklist_updated_at ON public.blacklist IS 'trigger to set value of column "updated_at" to current timestamp on row update';
+CREATE TRIGGER set_public_hospital_coordinator_updated_at BEFORE UPDATE ON public.hospital_coordinator FOR EACH ROW EXECUTE FUNCTION public.set_current_timestamp_updated_at();
+COMMENT ON TRIGGER set_public_hospital_coordinator_updated_at ON public.hospital_coordinator IS 'trigger to set value of column "updated_at" to current timestamp on row update';
+CREATE TRIGGER set_public_hospital_updated_at BEFORE UPDATE ON public.hospital FOR EACH ROW EXECUTE FUNCTION public.set_current_timestamp_updated_at();
+COMMENT ON TRIGGER set_public_hospital_updated_at ON public.hospital IS 'trigger to set value of column "updated_at" to current timestamp on row update';
+CREATE TRIGGER set_public_period_updated_at BEFORE UPDATE ON public.period FOR EACH ROW EXECUTE FUNCTION public.set_current_timestamp_updated_at();
+COMMENT ON TRIGGER set_public_period_updated_at ON public.period IS 'trigger to set value of column "updated_at" to current timestamp on row update';
+CREATE TRIGGER set_public_special_shift_updated_at BEFORE UPDATE ON public.special_shift FOR EACH ROW EXECUTE FUNCTION public.set_current_timestamp_updated_at();
+COMMENT ON TRIGGER set_public_special_shift_updated_at ON public.special_shift IS 'trigger to set value of column "updated_at" to current timestamp on row update';
+CREATE TRIGGER set_public_volunteer_shift_updated_at BEFORE UPDATE ON public.volunteer_shift FOR EACH ROW EXECUTE FUNCTION public.set_current_timestamp_updated_at();
+COMMENT ON TRIGGER set_public_volunteer_shift_updated_at ON public.volunteer_shift IS 'trigger to set value of column "updated_at" to current timestamp on row update';
+CREATE TRIGGER set_public_volunteer_updated_at BEFORE UPDATE ON public.volunteer FOR EACH ROW EXECUTE FUNCTION public.set_current_timestamp_updated_at();
+COMMENT ON TRIGGER set_public_volunteer_updated_at ON public.volunteer IS 'trigger to set value of column "updated_at" to current timestamp on row update';
+ALTER TABLE ONLY public.blacklist
+    ADD CONSTRAINT blacklist_volunteer_id_fkey FOREIGN KEY (volunteer_id) REFERENCES public.volunteer(uid) ON UPDATE RESTRICT ON DELETE RESTRICT;
 ALTER TABLE ONLY public.hospital_coordinator
     ADD CONSTRAINT hospital_coordinator_hospital_id_fkey FOREIGN KEY (hospital_id) REFERENCES public.hospital(uid) ON UPDATE CASCADE ON DELETE CASCADE;
 ALTER TABLE ONLY public.period
