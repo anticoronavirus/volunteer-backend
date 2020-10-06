19773a7df197b08ec928936e8e04c3367c0eae2a
Mike Koltsov <mikekoltsov@gmail.com>
2020-10-06 23:45:33 +0300

    add new professions

---
+++ hasura/migrations/1602017062429_extend_profession_list/up.sql
@@ -0,0 +1,5 @@
+insert into requirement(name) values('Хирургия');
+insert into requirement(name) values('Гнойная хирургия');
+insert into requirement(name) values('ОРИТ');
+insert into requirement(name) values('Кардиология');
+insert into requirement(name) values('Неврология');
